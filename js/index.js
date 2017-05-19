6778bbb224a2001f79d18ad7cd0c413b229450a2
Mathew Perrow <mathew.perrow@gmail.com>
2017-05-19 09:34:42 -0700

    Added personalized greeting
    Added, good morning, afternoon , or evening to top of the page. Also go
    rid of icons on project buttons, I'm going to rework in the future.

--- js/index.js
+++ js/index.js
@@ -43,17 +43,16 @@
 // 	$("#sqlIcon").slideToggle(".5s")
 // });
 
-// $(".date").ready(function(){
-// 	var time = Date();
-// 	// console.log(d);
-// 	time = time.getHours();
-
-// 	console.log(time);
-// 	if (time <12){
-// 		$("#greeting").append("<p>Good Morning</p>");
-// 	} else if (time <17){
-// 		$("#greeting").append("<p>Good Afternoon</p>");
-// 	} else{
-// 		$("#greeting").append("<p>Good Evening</p>");
-// 	}
-// });
+$(".date").ready(function(){
+	var hours = new Date();
+	hours = hours.getHours();
+	
+	console.log(hours);
+	if (hours <12){
+		$("#greeting").append("<p>Good Morning</p>");
+	} else if (hours <17){
+		$("#greeting").append("<p>Good Afternoon</p>");
+	} else{
+		$("#greeting").append("<p>Good Evening</p>");
+	}
+});
